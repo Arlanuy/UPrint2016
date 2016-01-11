6074d67446db72725419fb2642340db52ce15e95
acedtalibong <acedtalibong@gmail.com>
2016-01-11 11:04:54 +0800

    Add shop model

--- uprint/app/models/shop.rb
+++ uprint/app/models/shop.rb
@@ -1,6 +1,6 @@
 class Shop < ActiveRecord::Base
 
   validates :shop_name, presence: true, uniqueness: true
-  validates :location, presence: true
   validates :email, presence: true
+  validates :location, presence: true
 end
