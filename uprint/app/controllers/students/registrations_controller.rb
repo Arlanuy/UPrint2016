65bd6ac272f550d8906a4574aa3959b7cd23d07f
DEGABGED <franciszac.dlc@gmail.com>
2016-03-16 23:49:47 +0800

    Fix student and shop registration from landing page

--- uprint/app/controllers/students/registrations_controller.rb
+++ uprint/app/controllers/students/registrations_controller.rb
@@ -1,63 +1,21 @@
 class Students::RegistrationsController < Devise::RegistrationsController
-# before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
-  ## Please check this link on reference to this fix: http://stackoverflow.com/questions/9631432/rails3-devise-signup-filter-chain-halted-as-require-no-authentication-rendered
-  ## This line and replacement controller was done in response to the error replicated above. The error happened because there is already a user logged in, and a user can't sign up while signed in.
-  skip_before_filter :require_no_authentication, only: [:new, :create]
+    before_filter :configure_permitted_parameters
 
-  # GET /resource/sign_up
-  def new
-    super
-  end
+    def create
+        super
+        @student.errors.messages.each do |e|
+            p e
+        end
+    end
 
-  # POST /resource
-  def create
-    super
-  end
+    protected
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up).push(:name,
+                                                    :student_number,
+                                                    :email,
+                                                    :contact_number,
+                                                    :password,
+                                                    :password_confirmation)
+    end
 end
