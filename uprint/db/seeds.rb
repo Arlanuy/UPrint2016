fdd5d1ba62357c907e9d95466e341bf38c0700f4
carlaraya <carl_angelo.araya@upd.edu.ph>
2016-03-24 16:38:52 +0800

    Fix seeds and add something important in readme

--- uprint/db/seeds.rb
+++ uprint/db/seeds.rb
@@ -1,2 +1,20 @@
-Shop.create([{ shop_name: 'Blessings', location: 'Shopping Center', email: 'blessings@gmail.com' },
-              { shop_name: 'DHolyBook', location: 'Shopping Center', email: 'holybook@gmail.com' }])
+shops = [
+  { shop_name: 'Blessings', location: 'Shopping Center', email: 'blessings@gmail.com', password: 'password', contact_number: '09061234567'},
+  { shop_name: 'DHolyBook', location: 'Shopping Center', email: 'holybook@gmail.com', password: 'password', contact_number: '09071234567' }
+]
+
+students = [
+  { name: 'Juan Dela Cruz', student_number: '201500000', email: 'jdc@gmail.com', password: 'password', contact_number: '09161234567'}
+]
+
+shops.each do |shop|
+  s = Shop.create(shop)
+  s.save
+  s.confirm
+end
+
+students.each do |student|
+  s = Student.create(student)
+  s.save
+  s.confirm
+end
