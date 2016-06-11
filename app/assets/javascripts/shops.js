057c5794a86b6ac0355bcc9bbe6943b73d7cc3f0
DEGABGED <franciszac.dlc@gmail.com>
2016-06-11 17:11:05 +0800

    Create user card partial, make the availability switch work in the partial, retab landing and _pov html files

--- app/assets/javascripts/shops.js
+++ app/assets/javascripts/shops.js
@@ -1,15 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $("#avail-switch").click(function() {
-	$.ajax({
-		url: 'available',
-		type: 'POST',
-		success: function(result, status, xhr) {
-			if($("#avail-switch").hasClass("on")) {
-				$("#avail-switch").addClass("off").removeClass("on");
-			} else {
-				$("#avail-switch").addClass("on").removeClass("off");
-			}
-		}
-	});
+  $.ajax({
+    url: '/shops/available',
+    type: 'POST',
+    success: function(result, status, xhr) {
+      if($("#avail-switch").hasClass("on")) {
+        $("#avail-switch").addClass("off").removeClass("on");
+      } else {
+        $("#avail-switch").addClass("on").removeClass("off");
+      }
+    }
+  });
 });
